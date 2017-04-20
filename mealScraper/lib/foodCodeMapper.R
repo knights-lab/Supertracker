3524547f3449704bc8917946b94c287ea4cefb2b
pvangay <zaubme@gmail.com>
2017-04-20 15:32:33 -0500

    fixed some bugs in foodcodemapper.r
    need to merge on mod code as well (weird thing about USDA), fixed
    trimming whitespace problems, and double quote problems

--- mealScraper/lib/foodCodeMapper.R
+++ mealScraper/lib/foodCodeMapper.R
@@ -19,17 +19,22 @@ opts <- parse_args(OptionParser(option_list=option_list),
 setwd(opts$directory)
 
 
-foods <- read.table( paste( getwd(), "output", "mealSummaryOutput.txt",sep="/"), header=TRUE, sep="\t", quote="")
-meals <- read.csv( paste( getwd(), "data", 'SuperTracker Foods Database 2017.csv', sep='/'))
-output <- merge( foods, meals, by.x = "Food", by.y = "foodname", all.x=TRUE, all.y=FALSE)
+foods <- read.table( paste( getwd(), "output", "mealSummaryOutput.txt",sep="/"), header=TRUE, sep="\t", quote="", as.is=T, strip.white=T)
+meals <- read.csv( paste( getwd(), "data", 'SuperTracker Foods Database 2017.csv', sep='/'), as.is=T, strip.white=T)
+output0 <- merge( foods, meals, by.x = "Food", by.y = "foodname", all.x=TRUE, all.y=FALSE)
 
-portions <- read.table( paste( getwd(), "data", "SuperTracker Foods Database 2017 - Portion Data Tab.txt", sep="/"), header=TRUE, sep="\t", quote="")
-portions$foodname <- as.character(portions$foodname)
-portions$foodname <- substr(portions$foodname, 2, nchar(portions$foodname)-1)
+portions <- read.table( paste( getwd(), "data", "SuperTracker Foods Database 2017 - Portion Data Tab.txt", sep="/"), header=TRUE, sep="\t", as.is=T, strip.white=T, quote="")
 
-output <- merge( output, subset(portions, select = c(foodname, foodcode, portioncode, portiondesc, default.portion, portionwgt)), by.x=c("foodcode", "Portion" ), by.y=c("foodcode", "portiondesc" ))
+# for any foods not defined, take them out and add them back in later
+new.foods <- output0[is.na(output0$foodcode),]
+output0 <- output0[!is.na(output0$foodcode),]
+
+output <- merge( output0, subset(portions, select = c(foodname, foodcode, modcode, portion_code, portiondesc, default.portion, portionwgt)), by.x=c("foodcode", "modcode", "Portion" ), by.y=c("foodcode", "modcode", "portiondesc" ), all.x=TRUE)
 output$grams <- (output$PortionAmt/as.numeric(output$default.portion))*output$portionwgt
-output <-subset(output, select=c(Date, Food, foodcode, portioncode, Portion, PortionAmt, default.portion, portionwgt, grams))
-# do a merge with the portion csv here too
+output <-subset(output, select=c(DATE, Food, foodcode, modcode, portion_code, Portion, PortionAmt, default.portion, portionwgt, grams))
 
-write.table(output, file=paste(getwd(), "output", opts$outputfile, sep="/"), sep="\t", eol="\n", quote=FALSE, row.names=F)
+NAS <- rep(NA, nrow(new.foods))
+finaloutput <- rbind(output, cbind(DATE=new.foods$DATE, Food=new.foods$Food, foodcode=NAS, modcode=NAS, portion_code=NAS, Portion=new.foods$Portion, PortionAmt=new.foods$PortionAmt, default.portion=NAS, portionwgt=NAS, grams=NAS))
+# add new.foods back in
+
+write.table(finaloutput, file=paste(getwd(), "output", opts$outputfile, sep="/"), sep="\t", eol="\n", quote=FALSE, row.names=F)
